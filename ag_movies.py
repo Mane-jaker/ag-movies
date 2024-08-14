636602bd836b76d7297851a4c971f936141ab43b
Mane <angelillo3581@gmail.com>
2024-08-13 21:12:33 -0600

    queso

--- ag_movies.py
+++ ag_movies.py
@@ -81,7 +81,7 @@ def mutacion(poblacion, df, mutacion_prob):
 def ordenar_poblacion_por_aptitud(evaluacion_aptitud):
     return sorted(evaluacion_aptitud, key=lambda x: x['fitness'], reverse=True)
 
-def poda(evaluacion_aptitud, tamano_poblacion_maxima, pelicula_no_deseada):
+def poda(evaluacion_aptitud, tamano_poblacion_maxima, peliculas_no_deseadas, peliculas_favoritas):
     poblacion_unica = []
     peliculas_vistas = set()
 
@@ -93,22 +93,36 @@ def poda(evaluacion_aptitud, tamano_poblacion_maxima, pelicula_no_deseada):
             titulos.add(pelicula['title'])
         return False
 
+    def contiene_peliculas_favoritas(lista):
+        titulos_favoritos = set(pelicula['title'] for pelicula in peliculas_favoritas)
+        for pelicula in lista:
+            if pelicula['title'] in titulos_favoritos:
+                return True
+        return False
+
     for entrada in evaluacion_aptitud:
         lista_tuple = tuple(tuple(pelicula.items()) for pelicula in entrada['individual'])
         if lista_tuple not in peliculas_vistas:
             # Verificar si la lista contiene películas duplicadas
             if not tiene_peliculas_duplicadas(entrada['individual']):
                 peliculas_vistas.add(lista_tuple)
-                poblacion_unica.append(entrada)
+                if not contiene_peliculas_favoritas(entrada['individual']):
+                    poblacion_unica.append(entrada)
 
-    poblacion_filtrada = [entrada for entrada in poblacion_unica if not any(pelicula_no_deseada in pelicula['title'] for pelicula in entrada['individual'])]
+    def contiene_peliculas_no_deseadas(lista):
+        for pelicula in lista:
+            if any(pelicula_no_deseada in pelicula['title'] for pelicula_no_deseada in peliculas_no_deseadas):
+                return True
+        return False
+
+    poblacion_filtrada = [entrada for entrada in poblacion_unica if not contiene_peliculas_no_deseadas(entrada['individual'])]
 
     poblacion_ordenada = ordenar_poblacion_por_aptitud(poblacion_filtrada)
 
     return [entrada['individual'] for entrada in poblacion_ordenada[:tamano_poblacion_maxima]]
 
 
-def algoritmo_genetico(df, preferencias, generaciones, crossover_prob, mutacion_prob, tamano_poblacion, num_peliculas, tamano_poblacion_maxima, pelicula_no_deseada):
+def algoritmo_genetico(df, preferencias, generaciones, crossover_prob, mutacion_prob, tamano_poblacion, num_peliculas, tamano_poblacion_maxima, peliculas_no_deseadas, peliculas_favoritas):
     historial_aptitud = {'mejor': [], 'promedio': [], 'peor': []}
     
     poblacion = generar_poblacion(df, tamano_poblacion, num_peliculas)
@@ -119,13 +133,15 @@ def algoritmo_genetico(df, preferencias, generaciones, crossover_prob, mutacion_
         nuevos_hijos = mutacion(hijos, df, mutacion_prob)
         nueva_poblacion = list(poblacion) + list(nuevos_hijos)
         fitness_evaluation = evaluar_aptitud_poblacion(nueva_poblacion, preferencias)
-        poblacion = poda(fitness_evaluation, tamano_poblacion_maxima, pelicula_no_deseada) 
+        poblacion = poda(fitness_evaluation, tamano_poblacion_maxima, peliculas_no_deseadas, peliculas_favoritas)
         last_fitness = evaluar_aptitud_poblacion(poblacion, preferencias)
         fitness_values = [individual['fitness'] for individual in last_fitness]
         historial_aptitud['mejor'].append(max(fitness_values))
         historial_aptitud['promedio'].append(np.mean(fitness_values))
         historial_aptitud['peor'].append(min(fitness_values))   
     return poblacion, last_fitness, historial_aptitud
+
+
 
 def mostrar_grafica(aptitudes):
     ventana_grafica = Toplevel(root)
@@ -150,33 +166,39 @@ def mostrar_grafica(aptitudes):
     canvas.draw()
     canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-# Función para ejecutar el algoritmo a partir de las preferencias ingresadas
 def ejecutar_algoritmo():
     try:
+        # Obtener preferencias del formulario
         genres = formatear_entrada(genre_entry.get().split(','))
         actors = actor_entry.get().split(',')
         directors = director_entry.get().split(',')
         duration = int(duration_entry.get())
 
-        peliculas_favoritas = favorite_movie_entry.get().split(',')
-        pelicula_no_deseada = unwanted_movie_entry.get()
-
-        generaciones = int(generations_entry.get())
-        crossover_prob = float(crossover_prob_entry.get())
-        mutacion_prob = float(mutation_prob_entry.get())
-        tamano_poblacion_inicial = int(initial_population_size_entry.get())
-        tamano_poblacion_maxima = int(max_population_size_entry.get())
-        num_peliculas = int(movies_per_list_entry.get())
-
-        for pelicula_favorita in peliculas_favoritas:
+        # Obtener y procesar películas favoritas
+        peliculas_favoritas = []
+        for pelicula_favorita in favorite_movie_entry.get().split(','):
             pelicula_favorita = pelicula_favorita.strip().title()
             pelicula = df[df['title'].str.contains(pelicula_favorita, case=False, na=False)]
             if not pelicula.empty:
                 pelicula = pelicula.iloc[0]
+                peliculas_favoritas.append(pelicula)
+                # Agregar género, actores y director de la película favorita a las preferencias
                 genres.extend(pelicula['genres'].split('|'))
                 actors.extend(pelicula['cast'].split('|'))
                 directors.append(pelicula['director'])
 
+        # Obtener película no deseada
+        peliculas_no_deseadas = unwanted_movie_entry.get().split(',')
+ 
+        # Obtener parámetros del formulario
+        generaciones = int(generations_entry.get())
+        crossover_prob = float(crossover_prob_entry.get())
+        mutacion_prob = float(mutacion_prob_entry.get())
+        tamano_poblacion_inicial = int(initial_population_size_entry.get())
+        tamano_poblacion_maxima = int(max_population_size_entry.get())
+        num_peliculas = int(movies_per_list_entry.get())
+
+        # Crear diccionario de preferencias
         preferencias = {
             'genres': genres,
             'actors': actors,
@@ -184,6 +206,7 @@ def ejecutar_algoritmo():
             'duration': duration
         }
         
+        # Ejecutar el algoritmo genético
         poblacion, resultados, historial_aptitud = algoritmo_genetico(
             df,
             preferencias,
@@ -193,36 +216,116 @@ def ejecutar_algoritmo():
             tamano_poblacion_inicial,
             num_peliculas,
             tamano_poblacion_maxima,
-            pelicula_no_deseada
+            peliculas_no_deseadas,
+            peliculas_favoritas
         )
         
         # Mostrar resultados
-        resultado_text.delete(1.0, tk.END)
+        lista_resultados.delete(1.0, tk.END)
         if poblacion:
             mejor_lista = poblacion[0]  # Asumiendo que la primera lista es la de mayor aptitud
-            resultado_text.insert(tk.END, f"Mejor Lista de Películas:\n\n")
+            lista_resultados.insert(tk.END, f"Mejor Lista de Películas:\n\n")
             for pelicula in mejor_lista:
-                resultado_text.insert(tk.END, f"Título: {pelicula['title']}\n")
-                resultado_text.insert(tk.END, f"Elenco: {pelicula['cast']}\n")
-                resultado_text.insert(tk.END, f"Director: {pelicula['director']}\n")
-                resultado_text.insert(tk.END, "-"*40 + "\n")
+                lista_resultados.insert(tk.END, f"Título: {pelicula['title']}\n")
+                lista_resultados.insert(tk.END, f"Generos: {pelicula['genres']}\n")
+                lista_resultados.insert(tk.END, f"Elenco: {pelicula['cast']}\n")
+                lista_resultados.insert(tk.END, f"Director: {pelicula['director']}\n")
+                lista_resultados.insert(tk.END, "-"*40 + "\n")
         else:
-            resultado_text.insert(tk.END, "No se encontraron listas de películas.")
+            lista_resultados.insert(tk.END, "No se encontraron listas de películas.")
 
         mostrar_grafica(historial_aptitud)
 
     except ValueError as e:
         messagebox.showerror("Error de Entrada", f"Verifica que todos los campos estén correctamente llenos.\nError: {e}")
 
+        
+# Clase AutocompleteEntry
+class AutocompleteEntry(ttk.Entry):
+    def __init__(self, lista, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lista = sorted(lista)
+        self.var = self["textvariable"]
+        if self.var == '':
+            self.var = self["textvariable"] = tk.StringVar()
+        self.var.trace_add("write", self.changed)
+        self.bind("<Return>", self.selection)
+        self.bind("<Up>", self.move_up)
+        self.bind("<Down>", self.move_down)
+        self.lb_up = False
+
+    def changed(self, name, index, mode):
+        if self.var.get() == '':
+            if self.lb_up:
+                self.lb.destroy()
+                self.lb_up = False
+        else:
+            words = self.comparison()
+            if words:
+                if not self.lb_up:
+                    self.lb = tk.Listbox(width=self["width"])
+                    self.lb.bind("<Double-Button-1>", self.selection)
+                    self.lb.bind("<Return>", self.selection)
+                    self.lb.bind("<Escape>", self.escape)
+                    self.lb.place(x=self.winfo_x(), y=self.winfo_y() + self.winfo_height())
+                    self.lb_up = True
+                self.lb.delete(0, tk.END)
+                for w in words:
+                    self.lb.insert(tk.END, w)
+            else:
+                if self.lb_up:
+                    self.lb.destroy()
+                    self.lb_up = False
+
+    def selection(self, event):
+        if self.lb_up:
+            self.var.set(self.lb.get(tk.ACTIVE))
+            self.lb.destroy()
+            self.lb_up = False
+            self.icursor(tk.END)
+
+    def escape(self, event):
+        if self.lb_up:
+            self.lb.destroy()
+            self.lb_up = False
+
+    def move_up(self, event):
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '0'
+            else:
+                index = self.lb.curselection()[0]
+            if index != '0':
+                self.lb.selection_clear(first=index)
+                index = str(int(index) - 1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def move_down(self, event):
+        if self.lb_up:
+            if self.lb.curselection() == ():
+                index = '-1'
+            else:
+                index = self.lb.curselection()[0]
+            if index != tk.END:
+                self.lb.selection_clear(first=index)
+                index = str(int(index) + 1)
+                self.lb.selection_set(first=index)
+                self.lb.activate(index)
+
+    def comparison(self):
+        pattern = self.var.get()
+        return [w for w in self.lista if w.lower().startswith(pattern.lower())]
+
 # Configuración de la ventana principal
 root = tk.Tk()
 root.title("Algoritmo Genético para Recomendaciones de Películas")
-root.geometry("900x600")
+root.geometry("1200x650")
 
 # Estilo de ttk
 style = ttk.Style()
 style.configure('TButton', background='lightblue')
-style.configure('TLabel', font=('Arial', 10))
+style.configure('TLabel', font=('Arial', 10), anchor='w')
 style.configure('TEntry', font=('Arial', 10))
 
 # Frames
@@ -232,79 +335,54 @@ frame_inputs.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 frame_results = ttk.Frame(root, padding="10")
 frame_results.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
-# Entradas de datos con valores predeterminados
-ttk.Label(frame_inputs, text="Géneros (separados por coma):").grid(row=0, column=0, sticky=tk.W)
-genre_entry = ttk.Entry(frame_inputs, width=50)
-genre_entry.grid(row=0, column=1)
-genre_entry.insert(0, 'Family')
+# Labels y Entry Widgets en frame_inputs con valores predeterminados
+def add_input_row(frame, label_text, widget, default_value, row):
+    ttk.Label(frame, text=label_text, anchor='w').grid(row=row, column=0, sticky='w', pady=2)
+    widget.grid(row=row, column=1, sticky='w', pady=2)
+    if default_value:
+        widget.insert(0, default_value)
 
-ttk.Label(frame_inputs, text="Actores (separados por coma):").grid(row=1, column=0, sticky=tk.W)
-actor_entry = ttk.Entry(frame_inputs, width=50)
-actor_entry.grid(row=1, column=1)
-actor_entry.insert(0, 'Johnny Depp')
-
-ttk.Label(frame_inputs, text="Directores (separados por coma):").grid(row=2, column=0, sticky=tk.W)
-director_entry = ttk.Entry(frame_inputs, width=50)
-director_entry.grid(row=2, column=1)
-director_entry.insert(0, 'Gore Verbinski')
-
-ttk.Label(frame_inputs, text="Duración deseada (minutos):").grid(row=3, column=0, sticky=tk.W)
+# Crear los widgets de entrada
+genre_entry = AutocompleteEntry(df['genres'].str.split('|').explode().unique(), frame_inputs, width=50)
+actor_entry = AutocompleteEntry(df['cast'].str.split('|').explode().unique(), frame_inputs, width=50)
+director_entry = AutocompleteEntry(df['director'].unique(), frame_inputs, width=50)
 duration_entry = ttk.Entry(frame_inputs, width=50)
-duration_entry.grid(row=3, column=1)
-duration_entry.insert(0, '110')
-
-ttk.Label(frame_inputs, text="Películas Favoritas (separadas por coma):").grid(row=4, column=0, sticky=tk.W)
-favorite_movie_entry = ttk.Entry(frame_inputs, width=50)
-favorite_movie_entry.grid(row=4, column=1)
-favorite_movie_entry.insert(0, 'Megamind, Pirates of the Caribbean')
-
-ttk.Label(frame_inputs, text="Película No Deseada (opcional):").grid(row=5, column=0, sticky=tk.W)
-unwanted_movie_entry = ttk.Entry(frame_inputs, width=50)
-unwanted_movie_entry.grid(row=5, column=1)
-unwanted_movie_entry.insert(0, 'The Good Dinosaur')
-
-ttk.Label(frame_inputs, text="Número de Generaciones:").grid(row=6, column=0, sticky=tk.W)
+favorite_movie_entry = AutocompleteEntry(df['title'].unique(), frame_inputs, width=50)
+unwanted_movie_entry = AutocompleteEntry(df['title'].unique(), frame_inputs, width=50)
 generations_entry = ttk.Entry(frame_inputs, width=50)
-generations_entry.grid(row=6, column=1)
-generations_entry.insert(0, '100')
-
-ttk.Label(frame_inputs, text="Probabilidad de Cruce:").grid(row=7, column=0, sticky=tk.W)
 crossover_prob_entry = ttk.Entry(frame_inputs, width=50)
-crossover_prob_entry.grid(row=7, column=1)
-crossover_prob_entry.insert(0, '0.8')
-
-ttk.Label(frame_inputs, text="Probabilidad de Mutación:").grid(row=8, column=0, sticky=tk.W)
-mutation_prob_entry = ttk.Entry(frame_inputs, width=50)
-mutation_prob_entry.grid(row=8, column=1)
-mutation_prob_entry.insert(0, '0.3')
-
-ttk.Label(frame_inputs, text="Tamaño de la Población Inicial:").grid(row=9, column=0, sticky=tk.W)
+mutacion_prob_entry = ttk.Entry(frame_inputs, width=50)
 initial_population_size_entry = ttk.Entry(frame_inputs, width=50)
-initial_population_size_entry.grid(row=9, column=1)
-initial_population_size_entry.insert(0, '10')
-
-ttk.Label(frame_inputs, text="Tamaño Máximo de la Población:").grid(row=10, column=0, sticky=tk.W)
 max_population_size_entry = ttk.Entry(frame_inputs, width=50)
-max_population_size_entry.grid(row=10, column=1)
-max_population_size_entry.insert(0, '20')
-
-ttk.Label(frame_inputs, text="Número de Películas por Lista:").grid(row=11, column=0, sticky=tk.W)
 movies_per_list_entry = ttk.Entry(frame_inputs, width=50)
-movies_per_list_entry.grid(row=11, column=1)
-movies_per_list_entry.insert(0, '5')
+
+# Añadir las filas de entrada al frame
+inputs = [
+    ("Géneros (separados por comas):", genre_entry, "Action,Comedy"),
+    ("Actores (separados por comas):", actor_entry, "Jhonny Depp"),
+    ("Directores (separados por comas):", director_entry, "Christopher Nolan"),
+    ("Duración preferida (minutos):", duration_entry, "120"),
+    ("Películas favoritas (separadas por comas):", favorite_movie_entry, "Inception,Interstellar"),
+    ("Películas no deseadas  (separadas por comas):", unwanted_movie_entry, "Titanic"),
+    ("Número de generaciones:", generations_entry, "100"),
+    ("Probabilidad de cruzamiento (0-1):", crossover_prob_entry, "0.8"),
+    ("Probabilidad de mutación (0-1):", mutacion_prob_entry, "0.4"),
+    ("Tamaño de la población inicial:", initial_population_size_entry, "10"),
+    ("Tamaño máximo de la población:", max_population_size_entry, "20"),
+    ("Número de películas por lista:", movies_per_list_entry, "10"),
+]
+
+for i, (label, widget, default) in enumerate(inputs):
+    add_input_row(frame_inputs, label, widget, default, i)
 
 # Botón para ejecutar el algoritmo
-ejecutar_button = ttk.Button(frame_inputs, text="Ejecutar Algoritmo", command=ejecutar_algoritmo)
-ejecutar_button.grid(row=12, column=0, columnspan=2, pady=10)
+ttk.Button(frame_inputs, text="Ejecutar Algoritmo", command=ejecutar_algoritmo, padding=10, width=40).grid(row=len(inputs), columnspan=2, pady=20)
 
-# Área de resultados
-resultado_text = scrolledtext.ScrolledText(frame_results, width=50, height=30)
-resultado_text.pack(fill=tk.BOTH, expand=True)
+# Área de texto para mostrar los resultados en frame_results
+lista_resultados = scrolledtext.ScrolledText(frame_results, width=80, height=37)
+lista_resultados.pack()
 
-# Ejecutar la interfaz
 root.mainloop()
-
-
 
 entradas = {
     'Family'
